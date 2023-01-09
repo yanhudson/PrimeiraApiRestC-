4c95579a208b9bd7827dabcd6b96c847e7e0bc35
yanhudson <yan.hudson@bandtec.com.br>
2023-01-09 20:59:17 -0300

    criando domain com cliente, e add referencias com o entityFramework e mapaendo tabela db cliente

---
+++ Dao/ClienteDao.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Dao
+{
+    public class ClienteDao : DbContext
+    {
+        public ClienteDao(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; } 
+    }
+}
---
+++ Domain/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo 50 caracteres")]
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public int Idade { get; set; }
+
+
+
+    }
+}
